0a0f958667053d3cf09e0acfbfb3735247ecab6a
Thomas Naeff <tnaeff@solventum.com>
2024-03-08 16:35:24 +0100

    all node types are iterable. Added 'print' method for visualization.

--- src/main/java/com/mmm/his/cer/utility/farser/ast/node/type/BooleanExpression.java
+++ src/main/java/com/mmm/his/cer/utility/farser/ast/node/type/BooleanExpression.java
@@ -5,15 +5,37 @@ package com.mmm.his.cer.utility.farser.ast.node.type;
  * boolean expression through recursion.
  *
  * @param <T> The type of context to be used for the terminal node execution.
+ *
  * @author Mike Funaro
  */
-public interface BooleanExpression<T> {
+public interface BooleanExpression<T> extends Iterable<BooleanExpression<T>> {
 
   /**
    * Evaluate an expression returning true or false based on tests against the operands sent in.
    *
    * @param context The context that will be used in the evaluation of the node.
-   * @return true or false.
+   * @return <code>true</code> or <code>false</code>.
    */
   boolean evaluate(T context);
+
+  /**
+   * Returns an iterator over the expression elements.<br>
+   * <br>
+   * For terminal nodes, <code>null</code> should not be returned but an empty iterator can be
+   * returned (<code>return new ExpressionIterator<>(this)</code> - this default implementation).
+   */
+  @Override
+  default LtrExpressionIterator<T> iterator() {
+    return new LtrExpressionIterator<>();
+  }
+
+  /**
+   * Returns a printable representation of the node.
+   *
+   * @return The printable form of this node
+   */
+  default String print() {
+    return toString();
+  }
+
 }
--- src/main/java/com/mmm/his/cer/utility/farser/ast/node/type/NonTerminal.java
+++ src/main/java/com/mmm/his/cer/utility/farser/ast/node/type/NonTerminal.java
@@ -4,20 +4,31 @@ package com.mmm.his.cer.utility.farser.ast.node.type;
  * This class represents a non-terminal node in the AST. These types of nodes will have a left and a
  * right child.
  *
- * @param <T> The type used in the terminal nodes.
+ * @param <R> The type used in the terminal nodes.
  * @author Mike Funaro
  */
-public abstract class NonTerminal<T> implements BooleanExpression<T> {
+public abstract class NonTerminal<R> implements BooleanExpression<R> {
 
-  protected BooleanExpression<T> left;
-  protected BooleanExpression<T> right;
+  protected BooleanExpression<R> left;
+  protected BooleanExpression<R> right;
 
-  public void setLeft(BooleanExpression<T> left) {
+  public void setLeft(BooleanExpression<R> left) {
     this.left = left;
   }
 
-  public void setRight(BooleanExpression<T> right) {
+  public void setRight(BooleanExpression<R> right) {
     this.right = right;
+  }
+
+  @Override
+  public LtrExpressionIterator<R> iterator() {
+    return new LtrExpressionIterator<>(left, right);
+  }
+
+  @Override
+  public String print() {
+    // A default printing behavior. Can be overridden by implementations if needed.
+    return getClass().getSimpleName().toUpperCase();
   }
 
   @Override
