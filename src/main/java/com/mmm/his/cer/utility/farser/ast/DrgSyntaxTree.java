0ca3df38976ab793cbd5aa45433bc77364c9d436
Thomas Naeff <tnaeff@solventum.com>
2024-03-08 17:11:11 +0100

    old parser and tree simply extend new generic ones

--- src/main/java/com/mmm/his/cer/utility/farser/ast/DrgSyntaxTree.java
+++ src/main/java/com/mmm/his/cer/utility/farser/ast/DrgSyntaxTree.java
@@ -1,53 +1,20 @@
 package com.mmm.his.cer.utility.farser.ast;
 
 import com.mmm.his.cer.utility.farser.ast.node.type.BooleanExpression;
-import com.mmm.his.cer.utility.farser.ast.node.type.LtrExpressionIterator;
-import com.mmm.his.cer.utility.farser.ast.node.type.NonTerminal;
-import com.mmm.his.cer.utility.farser.ast.parser.ExpressionResult;
 
 /**
  * Class that wraps a {@link BooleanExpression} and provides methods to evaluate it.
  *
  * @param <T> the type of context
  * @author Mike Funaro
+ * @deprecated Use {@link AbstractSyntaxTree} instead for a non-DRG specific named class version with the
+ *             exact same functionality.
  */
-public class DrgSyntaxTree<T> extends NonTerminal<T> implements Iterable<BooleanExpression<T>> {
-
-  private BooleanExpression<T> ast;
+@Deprecated
+public class DrgSyntaxTree<T> extends AbstractSyntaxTree<T> {
 
   public DrgSyntaxTree(BooleanExpression<T> ast) {
-    this.ast = ast;
-  }
-
-  public void setAst(BooleanExpression<T> ast) {
-    this.ast = ast;
-  }
-
-  @Override
-  public boolean evaluate(T context) {
-    return this.ast.evaluate(context);
-  }
-
-  /**
-   * Evaluate an expression that was previously built by the parser.
-   *
-   * @param context the context object that will be used in the evaluation.
-   * @return {@link ExpressionResult} ExpressionResult object which will have the data about the
-   *         outcome of the evaluation.
-   */
-  public ExpressionResult<T> evaluateExpression(T context) {
-    boolean evaluate = evaluate(context);
-    return new ExpressionResult<>(evaluate, context);
-  }
-
-  @Override
-  public LtrExpressionIterator<T> iterator() {
-    return this.ast.iterator();
-  }
-
-  @Override
-  public String print() {
-    return this.ast.print();
+    super(ast);
   }
 
 }
