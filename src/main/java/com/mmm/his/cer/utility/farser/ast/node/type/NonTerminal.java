eb461363ba0d6442fb7579ec0569d6a7a07b5985
Thomas Naeff <tnaeff@solventum.com>
2024-03-12 18:29:42 +0100

    generic param changes reverted

--- src/main/java/com/mmm/his/cer/utility/farser/ast/node/type/NonTerminal.java
+++ src/main/java/com/mmm/his/cer/utility/farser/ast/node/type/NonTerminal.java
@@ -6,24 +6,24 @@ import com.mmm.his.cer.utility.farser.ast.node.LtrExpressionIterator;
  * This class represents a non-terminal node in the AST. These types of nodes will have a left and a
  * right child.
  *
- * @param <R> The type used in the terminal nodes.
+ * @param <C> The node context type used in the terminal nodes.
  * @author Mike Funaro
  */
-public abstract class NonTerminal<R> implements BooleanExpression<R> {
+public abstract class NonTerminal<C> implements BooleanExpression<C> {
 
-  protected BooleanExpression<R> left;
-  protected BooleanExpression<R> right;
+  protected BooleanExpression<C> left;
+  protected BooleanExpression<C> right;
 
-  public void setLeft(BooleanExpression<R> left) {
+  public void setLeft(BooleanExpression<C> left) {
     this.left = left;
   }
 
-  public void setRight(BooleanExpression<R> right) {
+  public void setRight(BooleanExpression<C> right) {
     this.right = right;
   }
 
   @Override
-  public LtrExpressionIterator<R> iterator() {
+  public LtrExpressionIterator<C> iterator() {
     return new LtrExpressionIterator<>(left, right);
   }
 
