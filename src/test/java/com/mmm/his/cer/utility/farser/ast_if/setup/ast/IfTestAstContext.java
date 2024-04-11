3791f7e59ceebfbf0788c0019e1945a62ef0e306
Thomas Naeff <tnaeff@solventum.com>
2024-04-11 10:05:35 +0200

    if-test fix

--- src/test/java/com/mmm/his/cer/utility/farser/ast_if/setup/ast/IfTestAstContext.java
+++ src/test/java/com/mmm/his/cer/utility/farser/ast_if/setup/ast/IfTestAstContext.java
@@ -2,6 +2,7 @@ package com.mmm.his.cer.utility.farser.ast_if.setup.ast;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 /**
@@ -17,7 +18,7 @@ public class IfTestAstContext {
 
   public IfTestAstContext(String... trueExpressions) {
     this.trueExpressions = new HashSet<>(Arrays.asList(trueExpressions));
-    this.evaluatedExpressions = new HashSet<>();
+    this.evaluatedExpressions = new LinkedHashSet<>();
   }
 
 
