b20af9e6b927ab22f173dc0b44677808b820b4b3
Thomas Naeff <tnaeff@solventum.com>
2024-04-11 11:59:58 +0200

    More complex formulas and nested if-statements tested

--- src/test/java/com/mmm/his/cer/utility/farser/ast_if_complex/ComplexIfEvaluationTest.java
+++ src/test/java/com/mmm/his/cer/utility/farser/ast_if_complex/ComplexIfEvaluationTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 
 import com.mmm.his.cer.utility.farser.ast.AbstractSyntaxTree;
+import com.mmm.his.cer.utility.farser.ast.AbstractSyntaxTreePrinter;
 import com.mmm.his.cer.utility.farser.ast.node.type.NodeSupplier;
 import com.mmm.his.cer.utility.farser.ast.parser.AstDescentParser;
 import com.mmm.his.cer.utility.farser.ast.parser.ExpressionResult;
@@ -146,6 +147,84 @@ public class ComplexIfEvaluationTest {
 
     // It should evaluate the 'A = 5' expression, but not pass.
     assertThat(context.evaluatedExpressions, contains("A", "\"5\""));
+
+  }
+
+  @Test
+  public void testLargeFormula() throws Exception {
+    String input = "IF A & B < 2 & !C | D = 3 THEN X";
+    List<ComplexIfTestToken> tokens = Lexer.lex(ComplexIfTestTokenType.class, input, factory);
+
+    AstDescentParser<ComplexIfTestToken, ComplexIfTestTokenType, ComplexIfTestAstContext> parser =
+        new AstDescentParser<>(tokens.iterator(), defaultNodeSupplier);
+    AbstractSyntaxTree<ComplexIfTestAstContext> ast = parser.buildTree();
+
+    System.out.println(AbstractSyntaxTreePrinter.printTree(ast));
+
+    ExpressionResult<ComplexIfTestAstContext> evaluation = ast.evaluateExpression(
+        new ComplexIfTestAstContext(vars -> {
+          vars.put("A", true);
+          vars.put("B", 1);
+          vars.put("C", false);
+          // vars.put("C", true);
+          // vars.put("D", 3);
+        }));
+    ComplexIfTestAstContext context = evaluation.getContext();
+
+    assertThat(evaluation.isMatched(), is(true));
+
+    assertThat(context.evaluatedExpressions, contains("A", "B", "2", "C", "X")); // , "D", "3"));
+
+  }
+
+  @Test
+  public void testLargeIfElseElseif() throws Exception {
+    String input = "IF A = 3 THEN X "
+        + "ELSE IF A > 3 THEN Y "
+        + "ELSE Z";
+    List<ComplexIfTestToken> tokens = Lexer.lex(ComplexIfTestTokenType.class, input, factory);
+
+    AstDescentParser<ComplexIfTestToken, ComplexIfTestTokenType, ComplexIfTestAstContext> parser =
+        new AstDescentParser<>(tokens.iterator(), defaultNodeSupplier);
+    AbstractSyntaxTree<ComplexIfTestAstContext> ast = parser.buildTree();
+
+    System.out.println(AbstractSyntaxTreePrinter.printTree(ast));
+
+    ExpressionResult<ComplexIfTestAstContext> evaluation = ast.evaluateExpression(
+        new ComplexIfTestAstContext(vars -> {
+          vars.put("A", 4);
+        }));
+    ComplexIfTestAstContext context = evaluation.getContext();
+
+    assertThat(evaluation.isMatched(), is(true));
+
+    assertThat(context.evaluatedExpressions, contains("A", "3", "A", "3", "Y"));
+
+  }
+
+  @Test
+  public void testNestedIf() throws Exception {
+    String input = "IF A = 3 THEN  "
+        + "  IF B > 3 THEN Y " // Nested IF
+        + "ELSE Z";
+    List<ComplexIfTestToken> tokens = Lexer.lex(ComplexIfTestTokenType.class, input, factory);
+
+    AstDescentParser<ComplexIfTestToken, ComplexIfTestTokenType, ComplexIfTestAstContext> parser =
+        new AstDescentParser<>(tokens.iterator(), defaultNodeSupplier);
+    AbstractSyntaxTree<ComplexIfTestAstContext> ast = parser.buildTree();
+
+    System.out.println(AbstractSyntaxTreePrinter.printTree(ast));
+
+    ExpressionResult<ComplexIfTestAstContext> evaluation = ast.evaluateExpression(
+        new ComplexIfTestAstContext(vars -> {
+          vars.put("A", 3);
+          vars.put("B", 4);
+        }));
+    ComplexIfTestAstContext context = evaluation.getContext();
+
+    assertThat(evaluation.isMatched(), is(true));
+
+    assertThat(context.evaluatedExpressions, contains("A", "3", "B", "3", "Y"));
 
   }
 
--- src/test/java/com/mmm/his/cer/utility/farser/ast_if_complex/setup/ast/ComplexIfTestAstContext.java
+++ src/test/java/com/mmm/his/cer/utility/farser/ast_if_complex/setup/ast/ComplexIfTestAstContext.java
@@ -1,9 +1,9 @@
 package com.mmm.his.cer.utility.farser.ast_if_complex.setup.ast;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.function.Consumer;
 
 /**
@@ -15,13 +15,13 @@ import java.util.function.Consumer;
 public class ComplexIfTestAstContext {
 
   public final Map<String, Object> variablesContent;
-  public final Set<String> evaluatedExpressions;
+  public final List<String> evaluatedExpressions;
 
   public ComplexIfTestAstContext(Consumer<Map<String, Object>> variablesContentSupplier) {
     this.variablesContent = new HashMap<>();
     // Apply setup
     variablesContentSupplier.accept(variablesContent);
-    this.evaluatedExpressions = new LinkedHashSet<>();
+    this.evaluatedExpressions = new ArrayList<>();
   }
 
 
